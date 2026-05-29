337ae300cb35b47b82dedd9666f90a82e3db50d3
Rakesh kumar Mahendra Kumar <rakeshkumarmahendrakumar@Rakeshs-Mac-mini.local>
2026-05-29 12:17:09 -0700

    Add read/write STREAM variant

--- stream.c
+++ stream.c
@@ -176,30 +176,25 @@
 #define STREAM_TYPE double
 #endif
 
-static STREAM_TYPE	a[STREAM_ARRAY_SIZE+OFFSET],
-			b[STREAM_ARRAY_SIZE+OFFSET],
-			c[STREAM_ARRAY_SIZE+OFFSET];
+static STREAM_TYPE	a[STREAM_ARRAY_SIZE+OFFSET];
 
-static double	avgtime[4] = {0}, maxtime[4] = {0},
-		mintime[4] = {FLT_MAX,FLT_MAX,FLT_MAX,FLT_MAX};
+static double	avgtime[2] = {0}, maxtime[2] = {0},
+		mintime[2] = {FLT_MAX,FLT_MAX};
 
-static char	*label[4] = {"Copy:      ", "Scale:     ",
-    "Add:       ", "Triad:     "};
+static char	*label[2] = {"Write:     ", "Read:      "};
 
-static double	bytes[4] = {
-    2 * sizeof(STREAM_TYPE) * STREAM_ARRAY_SIZE,
-    2 * sizeof(STREAM_TYPE) * STREAM_ARRAY_SIZE,
-    3 * sizeof(STREAM_TYPE) * STREAM_ARRAY_SIZE,
-    3 * sizeof(STREAM_TYPE) * STREAM_ARRAY_SIZE
+static double	bytes[2] = {
+    sizeof(STREAM_TYPE) * STREAM_ARRAY_SIZE,
+    sizeof(STREAM_TYPE) * STREAM_ARRAY_SIZE
     };
+
+static volatile STREAM_TYPE read_sink;
 
 extern double mysecond();
 extern void checkSTREAMresults();
 #ifdef TUNED
-extern void tuned_STREAM_Copy();
-extern void tuned_STREAM_Scale(STREAM_TYPE scalar);
-extern void tuned_STREAM_Add();
-extern void tuned_STREAM_Triad(STREAM_TYPE scalar);
+extern void tuned_STREAM_Write(STREAM_TYPE value);
+extern STREAM_TYPE tuned_STREAM_Read();
 #endif
 #ifdef _OPENMP
 extern int omp_get_num_threads();
@@ -211,8 +206,9 @@ main()
     int			BytesPerWord;
     int			k;
     ssize_t		j;
-    STREAM_TYPE		scalar;
-    double		t, times[4][NTIMES];
+    STREAM_TYPE		read_sum;
+    STREAM_TYPE		write_value;
+    double		t, times[2][NTIMES];
 
     /* --- SETUP --- determine precision and check timing --- */
 
@@ -237,8 +233,8 @@ main()
 	BytesPerWord * ( (double) STREAM_ARRAY_SIZE / 1024.0/1024.0),
 	BytesPerWord * ( (double) STREAM_ARRAY_SIZE / 1024.0/1024.0/1024.0));
     printf("Total memory required = %.1f MiB (= %.1f GiB).\n",
-	(3.0 * BytesPerWord) * ( (double) STREAM_ARRAY_SIZE / 1024.0/1024.),
-	(3.0 * BytesPerWord) * ( (double) STREAM_ARRAY_SIZE / 1024.0/1024./1024.));
+	BytesPerWord * ( (double) STREAM_ARRAY_SIZE / 1024.0/1024.),
+	BytesPerWord * ( (double) STREAM_ARRAY_SIZE / 1024.0/1024./1024.));
     printf("Each kernel will be executed %d times.\n", NTIMES);
     printf(" The *best* time for each kernel (excluding the first iteration)\n"); 
     printf(" will be used to compute the reported bandwidth.\n");
@@ -267,8 +263,6 @@ main()
 #pragma omp parallel for
     for (j=0; j<STREAM_ARRAY_SIZE; j++) {
 	    a[j] = 1.0;
-	    b[j] = 2.0;
-	    c[j] = 0.0;
 	}
 
     printf(HLINE);
@@ -303,55 +297,37 @@ main()
     
     /*	--- MAIN LOOP --- repeat test cases NTIMES times --- */
 
-    scalar = 3.0;
     for (k=0; k<NTIMES; k++)
 	{
+	write_value = (STREAM_TYPE) (k + 1);
 	times[0][k] = mysecond();
 #ifdef TUNED
-        tuned_STREAM_Copy();
+        tuned_STREAM_Write(write_value);
 #else
 #pragma omp parallel for
 	for (j=0; j<STREAM_ARRAY_SIZE; j++)
-	    c[j] = a[j];
+	    a[j] = write_value;
 #endif
 	times[0][k] = mysecond() - times[0][k];
 	
+	read_sum = 0.0;
 	times[1][k] = mysecond();
 #ifdef TUNED
-        tuned_STREAM_Scale(scalar);
+        read_sum = tuned_STREAM_Read();
 #else
-#pragma omp parallel for
+#pragma omp parallel for reduction(+:read_sum)
 	for (j=0; j<STREAM_ARRAY_SIZE; j++)
-	    b[j] = scalar*c[j];
+	    read_sum += a[j];
 #endif
 	times[1][k] = mysecond() - times[1][k];
-	
-	times[2][k] = mysecond();
-#ifdef TUNED
-        tuned_STREAM_Add();
-#else
-#pragma omp parallel for
-	for (j=0; j<STREAM_ARRAY_SIZE; j++)
-	    c[j] = a[j]+b[j];
-#endif
-	times[2][k] = mysecond() - times[2][k];
-	
-	times[3][k] = mysecond();
-#ifdef TUNED
-        tuned_STREAM_Triad(scalar);
-#else
-#pragma omp parallel for
-	for (j=0; j<STREAM_ARRAY_SIZE; j++)
-	    a[j] = b[j]+scalar*c[j];
-#endif
-	times[3][k] = mysecond() - times[3][k];
+	read_sink = read_sum;
 	}
 
     /*	--- SUMMARY --- */
 
     for (k=1; k<NTIMES; k++) /* note -- skip first iteration */
 	{
-	for (j=0; j<4; j++)
+	for (j=0; j<2; j++)
 	    {
 	    avgtime[j] = avgtime[j] + times[j][k];
 	    mintime[j] = MIN(mintime[j], times[j][k]);
@@ -360,7 +336,7 @@ main()
 	}
     
     printf("Function    Best Rate MB/s  Avg time     Min time     Max time\n");
-    for (j=0; j<4; j++) {
+    for (j=0; j<2; j++) {
 		avgtime[j] = avgtime[j]/(double)(NTIMES-1);
 
 		printf("%s%12.1f  %11.6f  %11.6f  %11.6f\n", label[j],
@@ -432,42 +408,22 @@ double mysecond()
 #endif
 void checkSTREAMresults ()
 {
-	STREAM_TYPE aj,bj,cj,scalar;
-	STREAM_TYPE aSumErr,bSumErr,cSumErr;
-	STREAM_TYPE aAvgErr,bAvgErr,cAvgErr;
+	STREAM_TYPE aj;
+	STREAM_TYPE aSumErr;
+	STREAM_TYPE aAvgErr;
 	double epsilon;
 	ssize_t	j;
-	int	k,ierr,err;
+	int	ierr,err;
 
-    /* reproduce initialization */
-	aj = 1.0;
-	bj = 2.0;
-	cj = 0.0;
-    /* a[] is modified during timing check */
-	aj = 2.0E0 * aj;
-    /* now execute timing loop */
-	scalar = 3.0;
-	for (k=0; k<NTIMES; k++)
-        {
-            cj = aj;
-            bj = scalar*cj;
-            cj = aj+bj;
-            aj = bj+scalar*cj;
-        }
+	/* The write kernel stores k+1 each iteration, so the final value is NTIMES. */
+	aj = (STREAM_TYPE) NTIMES;
 
     /* accumulate deltas between observed and expected results */
 	aSumErr = 0.0;
-	bSumErr = 0.0;
-	cSumErr = 0.0;
 	for (j=0; j<STREAM_ARRAY_SIZE; j++) {
 		aSumErr += abs(a[j] - aj);
-		bSumErr += abs(b[j] - bj);
-		cSumErr += abs(c[j] - cj);
-		// if (j == 417) printf("Index 417: c[j]: %f, cj: %f\n",c[j],cj);	// MCCALPIN
 	}
 	aAvgErr = aSumErr / (STREAM_TYPE) STREAM_ARRAY_SIZE;
-	bAvgErr = bSumErr / (STREAM_TYPE) STREAM_ARRAY_SIZE;
-	cAvgErr = cSumErr / (STREAM_TYPE) STREAM_ARRAY_SIZE;
 
 	if (sizeof(STREAM_TYPE) == 4) {
 		epsilon = 1.e-6;
@@ -499,87 +455,35 @@ void checkSTREAMresults ()
 		}
 		printf("     For array a[], %d errors were found.\n",ierr);
 	}
-	if (abs(bAvgErr/bj) > epsilon) {
-		err++;
-		printf ("Failed Validation on array b[], AvgRelAbsErr > epsilon (%e)\n",epsilon);
-		printf ("     Expected Value: %e, AvgAbsErr: %e, AvgRelAbsErr: %e\n",bj,bAvgErr,abs(bAvgErr)/bj);
-		printf ("     AvgRelAbsErr > Epsilon (%e)\n",epsilon);
-		ierr = 0;
-		for (j=0; j<STREAM_ARRAY_SIZE; j++) {
-			if (abs(b[j]/bj-1.0) > epsilon) {
-				ierr++;
-#ifdef VERBOSE
-				if (ierr < 10) {
-					printf("         array b: index: %ld, expected: %e, observed: %e, relative error: %e\n",
-						j,bj,b[j],abs((bj-b[j])/bAvgErr));
-				}
-#endif
-			}
-		}
-		printf("     For array b[], %d errors were found.\n",ierr);
-	}
-	if (abs(cAvgErr/cj) > epsilon) {
-		err++;
-		printf ("Failed Validation on array c[], AvgRelAbsErr > epsilon (%e)\n",epsilon);
-		printf ("     Expected Value: %e, AvgAbsErr: %e, AvgRelAbsErr: %e\n",cj,cAvgErr,abs(cAvgErr)/cj);
-		printf ("     AvgRelAbsErr > Epsilon (%e)\n",epsilon);
-		ierr = 0;
-		for (j=0; j<STREAM_ARRAY_SIZE; j++) {
-			if (abs(c[j]/cj-1.0) > epsilon) {
-				ierr++;
-#ifdef VERBOSE
-				if (ierr < 10) {
-					printf("         array c: index: %ld, expected: %e, observed: %e, relative error: %e\n",
-						j,cj,c[j],abs((cj-c[j])/cAvgErr));
-				}
-#endif
-			}
-		}
-		printf("     For array c[], %d errors were found.\n",ierr);
-	}
 	if (err == 0) {
-		printf ("Solution Validates: avg error less than %e on all three arrays\n",epsilon);
+		printf ("Solution Validates: avg error less than %e on the write array\n",epsilon);
 	}
 #ifdef VERBOSE
 	printf ("Results Validation Verbose Results: \n");
-	printf ("    Expected a(1), b(1), c(1): %f %f %f \n",aj,bj,cj);
-	printf ("    Observed a(1), b(1), c(1): %f %f %f \n",a[1],b[1],c[1]);
-	printf ("    Rel Errors on a, b, c:     %e %e %e \n",abs(aAvgErr/aj),abs(bAvgErr/bj),abs(cAvgErr/cj));
+	printf ("    Expected a(1): %f \n",aj);
+	printf ("    Observed a(1): %f \n",a[1]);
+	printf ("    Rel Error on a: %e \n",abs(aAvgErr/aj));
 #endif
 }
 
 #ifdef TUNED
 /* stubs for "tuned" versions of the kernels */
-void tuned_STREAM_Copy()
+void tuned_STREAM_Write(STREAM_TYPE value)
 {
 	ssize_t j;
 #pragma omp parallel for
         for (j=0; j<STREAM_ARRAY_SIZE; j++)
-            c[j] = a[j];
+            a[j] = value;
 }
 
-void tuned_STREAM_Scale(STREAM_TYPE scalar)
+STREAM_TYPE tuned_STREAM_Read()
 {
 	ssize_t j;
-#pragma omp parallel for
+	STREAM_TYPE read_sum = 0.0;
+#pragma omp parallel for reduction(+:read_sum)
 	for (j=0; j<STREAM_ARRAY_SIZE; j++)
-	    b[j] = scalar*c[j];
-}
-
-void tuned_STREAM_Add()
-{
-	ssize_t j;
-#pragma omp parallel for
-	for (j=0; j<STREAM_ARRAY_SIZE; j++)
-	    c[j] = a[j]+b[j];
-}
-
-void tuned_STREAM_Triad(STREAM_TYPE scalar)
-{
-	ssize_t j;
-#pragma omp parallel for
-	for (j=0; j<STREAM_ARRAY_SIZE; j++)
-	    a[j] = b[j]+scalar*c[j];
+	    read_sum += a[j];
+	return read_sum;
 }
 /* end of stubs for the "tuned" versions of the kernels */
 #endif
